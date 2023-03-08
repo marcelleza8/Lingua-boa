0b41c4cfbce64bb6080976860a4b05b1d903d34e
MarceloHFilho <contato@sferadigital.info>
2023-03-08 19:08:02 -0300

    Melhorias

--- src/components/Formularios/InputText.vue
+++ src/components/Formularios/InputText.vue
@@ -1,18 +1,28 @@
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import { defineComponent, ref, onMounted } from "vue";
 
 export default defineComponent({
   name: "InputText",
-  setup(props) {
+  setup(props, context) {
     const idError = ref(false);
+
+    const handleInput = ($e: any) => {
+      context.emit("update:modelValue", $e.target.value);
+    };
+
     return {
       idError,
+      handleInput,
     };
   },
   props: {
     label: {
+      type: String,
       required: true,
       default: "",
+    },
+    modelValue: {
+      required: true,
     },
   },
 });
@@ -22,6 +32,8 @@ export default defineComponent({
   <input
     id="id"
     type="text"
+    :value="modelValue"
+    @input="handleInput"
     :class="{ 'border-red-500': idError }"
     class="focus:shadow-outline w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 shadow focus:outline-none"
     :placeholder="label"
--- src/pages/EditorDicionarioPage.vue
+++ src/pages/EditorDicionarioPage.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import { defineComponent, ref, computed } from "vue";
 import Form from "../components/Formularios/Form.vue";
 import InputText from "../components/Formularios/InputText.vue";
 import { saveAs } from "file-saver";
@@ -10,62 +10,121 @@ export default defineComponent({
   setup() {
     const dicionario = ref(dicionario_data);
 
+    const editarPalavra = ref({});
+    const significados = ref("");
+
+    const salvando = ref(false);
+
     const salvar = () => {
       const jsonData = JSON.stringify(dicionario.value);
       const blob = new Blob([jsonData], { type: "application/json" });
       saveAs(blob, "data.json");
     };
 
+    const editarItem = (palavra: any) => {
+      const editar = dicionario.value.find((item) => palavra.id === item.id);
+      if (editar) {
+        editarPalavra.value = editar;
+
+        console.log(significados.value);
+        significados.value = editarPalavra.value.significados.join(",");
+        console.log(typeof significados.value);
+      } else {
+        alert("Não encontrado");
+      }
+    };
+
+    const salvarItem = () => {
+      salvando.value = true;
+      const indice = dicionario.value.findIndex(
+        (item) => editarPalavra.value.id === item.id,
+      );
+
+      let signifs = significados.value.split(",");
+
+      editarPalavra.value.significados = signifs
+        .map((i: string) => i.trim())
+        .filter((i: string) => i.length);
+
+      dicionario.value[indice] = editarPalavra.value;
+      editarPalavra.value = {};
+      salvando.value = false;
+    };
+
+    const totalPalavras = computed(() => dicionario.value.length);
+
     return {
       dicionario,
+      editarPalavra,
+      salvando,
+      significados,
+      totalPalavras,
       salvar,
+      editarItem,
+      salvarItem,
     };
   },
   components: { Form, InputText },
 });
 </script>
 <template>
-  <h1>Editor de Dicionário</h1>
+  <h1>Editor de Dicionário (Total de Palavras: {{ totalPalavras }})</h1>
   <div class="px-2">
-    <div>
+    <div v-if="Object.keys(editarPalavra).length">
+      <Form>
+        <h1>Editando {{ editarPalavra.palavra }}</h1>
+        <InputText v-model="editarPalavra.palavra" label="Palavra" />
+        <InputText
+          v-model="significados"
+          label="Significados (separados por vírgula)"
+        />
+        <div class="mt-3">
+          <button
+            :disabled="salvando"
+            @click="salvarItem"
+            class="rounded-lg border border-suikiri-700 px-2 py-1 disabled:bg-gray-700"
+          >
+            Salvar
+          </button>
+          <span v-if="salvando"
+            >Salvando Palavra, aguarde um instante ....</span
+          >
+        </div>
+      </Form>
+    </div>
+    <div v-else>
       <button
         @click="salvar"
         class="rounded-md border border-orange-peel-500 px-2 py-1"
       >
         Salvar como JSON
       </button>
+
+      <table class="w-full table-auto">
+        <thead>
+          <tr>
+            <th class="px-4 py-2">Palavra</th>
+            <th class="px-4 py-2">Significados</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in dicionario" :key="item.id">
+            <td class="font-bold">{{ item.palavra }}</td>
+            <td class="space-x-2">
+              <span
+                class="rounded bg-suikiri-700 px-2 py-1 font-bold text-orange-peel-400"
+                v-for="significado in item.significados"
+                >{{ significado }}</span
+              >
+            </td>
+            <td>
+              <button @click="editarItem(item)">Ed</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-    <table class="w-full table-auto">
-      <thead>
-        <tr>
-          <th class="px-4 py-2">Palavra</th>
-          <th class="px-4 py-2">Significados</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="item in dicionario" :key="item.id">
-          <td>{{ item.palavra }}</td>
-          <td>
-            <span
-              class="rounded bg-slate-100 px-2 py-1"
-              v-for="significado in item.significados"
-              >{{ significado }}</span
-            >
-          </td>
-          <td>
-            <button>Ed</button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-  <div v-if="false">
-    <Form>
-      <h1>Formulário</h1>
-      <InputText label="ID" />
-      <InputText label="Palavra" />
-    </Form>
   </div>
 </template>
 

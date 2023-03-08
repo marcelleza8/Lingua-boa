7229fb79a07b5172aee9d884bf99c84d52f91c12
MarceloHFilho <contato@sferadigital.info>
2023-03-08 14:15:22 -0300

    Atualização do dicionario alterado sistema de rotas.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,19 +1,20 @@
 import { createRouter, createWebHistory } from "vue-router";
+import EditorDicionarioPageVue from "../pages/EditorDicionarioPage.vue";
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes:[
-        {
-            path: '/',
-            name:"home",
-            component: () => import('../pages/HomePage.vue')
-        },
-        {
-            path: '/editor-dicionario',
-            name: 'editor-dicionario',
-            component: () => import('../pages/EditorDicionarioPage.vue')
-        }
-    ]
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: () => import("../pages/HomePage.vue"),
+    },
+    {
+      path: "/editor-dicionario",
+      name: "editor-dicionario",
+      component: EditorDicionarioPageVue,
+    },
+  ],
 });
 
 export default router;

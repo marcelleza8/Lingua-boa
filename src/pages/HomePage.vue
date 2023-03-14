116dffbab748732c2353c1b01ddf9a6236311bfc
MarceloHFilho <contato@sferadigital.info>
2023-03-14 16:35:29 -0300

    Add arquivos de modelo de som

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,11 +1,29 @@
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, onMounted } from "vue";
 import Header from "../components/Header.vue";
 import ResultList from "../components/ResultList.vue";
 
+// import { audio as song } from "../data/audio";
+
 export default defineComponent({
   name: "HomePage",
-  setup() {},
+  setup() {
+    /* onMounted(() => {
+      const audioContext = new window.AudioContext();
+      console.log(audioContext);
+
+      // Criar um elemento de áudio
+      const audio = new Audio();
+
+      // Definir o atributo src como a string base64
+      audio.src = "data:audio/mp3;base64," + song;
+
+      console.log(audio);
+
+      // Reproduzir o áudio
+      audio.play();
+    }); */
+  },
   components: { Header, ResultList },
 });
 </script>

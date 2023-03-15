fa9b7b91bab6a195167b58f2f85547a24ee166a5
MarceloHFilho <contato@sferadigital.info>
2023-03-15 16:45:11 -0300

    Incluido compartilhamento

--- src/components/ResultCard.vue
+++ src/components/ResultCard.vue
@@ -1,21 +1,30 @@
 <script lang="ts">
 // @ts-nocheck
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import MeanigList from "./MeanigList.vue";
 import Example from "./Example.vue";
 import Synonym from "./Synonym.vue";
 import Antonym from "./Antonym.vue";
 import ShareIcon from "./Icons/ShareIcon.vue";
+import WhatsappIcon from "./Icons/WhatsappIcon.vue";
+import InputGroup from "./DaisyUI/InputGroup.vue";
+
+import { generateRandomID } from "../utils";
+import { LinkIcon } from "@heroicons/vue/24/solid";
 
 export default defineComponent({
   name: "ResultCard",
   setup() {
+    const modalCompartilhar = ref(false);
+    const id = ref(generateRandomID());
     const compartilhar = () => {
-      alert("Em breve");
+      modalCompartilhar.value = !modalCompartilhar.value;
     };
 
     return {
       compartilhar,
+      modalCompartilhar,
+      id,
     };
   },
   props: {
@@ -29,6 +38,9 @@ export default defineComponent({
     Example,
     Synonym,
     Antonym,
+    InputGroup,
+    LinkIcon,
+    WhatsappIcon,
   },
 });
 </script>
@@ -42,6 +54,32 @@ export default defineComponent({
       <div class="flex items-center">
         <div>
           <ShareIcon @click="compartilhar" class="w-6" />
+          <input
+            type="checkbox"
+            :checked="modalCompartilhar"
+            @click="compartilhar"
+            :id="id"
+            class="modal-toggle"
+          />
+          <label :for="id" class="modal cursor-pointer">
+            <label class="modal-box relative" for="">
+              <h3 class="text-lg font-bold">Compartilhar esse verbete</h3>
+              <p class="space-y-7 py-4">
+                <div>
+                  <a target="_blank" href="https://wa.me?text=Veja só o que esse verbete significa em *Nheengatu*: http://teste.com" class=" inline-block border border-green-300 p-2 rounded-full cursor-pointer">
+                    <WhatsappIcon class="h-10 w-10 " />
+                  </a>
+                </div>
+                <InputGroup :readonly="true" value="http://teste.com">
+                  <template #icon>
+                    <div class="h-full w-full p-2">
+                      <LinkIcon />
+                    </div>
+                  </template>
+                </InputGroup>
+              </p>
+            </label>
+          </label>
         </div>
       </div>
     </div>
@@ -56,3 +94,9 @@ export default defineComponent({
     <!-- <Antonym mean="{response}" /> -->
   </div>
 </template>
+
+<style>
+.modal-box{
+  @apply w-auto;
+}
+</style>

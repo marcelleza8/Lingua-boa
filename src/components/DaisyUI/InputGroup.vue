c05eb2d3341e2621a139025382a2a5b41bc54cd6
MarceloHFilho <contato@sferadigital.info>
2023-03-15 16:42:09 -0300

    Add icones e inputgroup de Daisy

---
+++ src/components/DaisyUI/InputGroup.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "InputGroup",
+  props: {
+    value: {
+      default: "",
+    },
+    readonly: {
+      default: false,
+      type: Boolean,
+    },
+  },
+});
+</script>
+
+<template>
+  <div class="form-control inline-block">
+    <div class="input-group w-full">
+      <input
+        type="text"
+        placeholder=""
+        :readonly="readonly"
+        :value="value"
+        class="w-full focus:ring-0"
+      />
+      <button class="btn-square btn border-transparent">
+        <slot name="icon"></slot>
+      </button>
+    </div>
+  </div>
+</template>
